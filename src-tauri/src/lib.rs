f2e52cca050253f8dbf7719758b5d7f98ce20891
sena-nana <34237511+sena-nana@users.noreply.github.com>
2026-06-29 21:01:38 +0800

    裁剪模板未使用依赖

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_lilia::init())
         .run(tauri::generate_context!())
